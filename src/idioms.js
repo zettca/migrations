d8ce681c687d83a9294af44c4bad7629422ff684
Bruno <zettca@gmail.com>
2018-11-26 02:37:38 +0000

    Implement sample scatterplot Implement sample line chart Refactor code

--- src/idioms.js
+++ src/idioms.js
@@ -8,22 +8,144 @@ export default {
   drawLines: drawLines,
 };
 
-const chordData = [
-  [19, 11, 19, 14, 15, 14, 13, 17],
-  [5, 9, 5, 13, 0, 16, 2, 9],
-  [12, 18, 12, 2, 1, 6, 13, 8],
-  [11, 12, 18, 5, 19, 6, 16, 18],
-  [19, 20, 17, 7, 16, 5, 9, 12],
-  [13, 18, 15, 6, 12, 14, 11, 7],
-  [10, 5, 0, 19, 18, 1, 1, 5],
-  [20, 10, 7, 7, 13, 6, 1, 20]];
-
 export function drawPlot(id, width, height, data) {
-  return undefined;
+  const svg = makeSVG(id, width, height);
+  const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+
+  const x = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.x)).nice()
+    .range([margin.left, width - margin.right]);
+
+  const y = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.y)).nice()
+    .range([height - margin.bottom, margin.top]);
+
+  function xAxis(g) {
+    g
+      .attr('transform', `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x))
+      .call(g => g.select('.domain').remove())
+      .call(g => g.append('text')
+        .attr('class', 'x-axis')
+        .attr('x', width - margin.right)
+        .attr('y', -4)
+        .text(data.x));
+  }
+
+  function yAxis(g) {
+    g
+      .attr('transform', `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y))
+      .call(g => g.select('.domain').remove())
+      .call(g => g.select('.tick:last-of-type text').clone()
+        .attr('class', 'y-axis')
+        .attr('x', 4)
+        .text(data.y));
+  }
+
+  function dodge(text, iterations = 300) {
+    const nodes = text.nodes();
+    const left = text => text.attr('text-anchor', 'start').attr('dy', '0.32em');
+    const right = text => text.attr('text-anchor', 'end').attr('dy', '0.32em');
+    const top = text => text.attr('text-anchor', 'middle').attr('dy', '0.0em');
+    const bottom = text => text.attr('text-anchor', 'middle').attr('dy', '0.8em');
+    const points = nodes.map(node => ({ fx: +node.getAttribute('x'), fy: +node.getAttribute('y') }));
+    const labels = points.map(({ fx, fy }) => ({ x: fx, y: fy }));
+    const links = points.map((source, i) => ({ source, target: labels[i] }));
+
+    const simulation = d3.forceSimulation(points.concat(labels))
+      .force('charge', d3.forceManyBody().distanceMax(80))
+      .force('link', d3.forceLink(links).distance(4).iterations(4))
+      .stop();
+
+    for (let i = 0; i < iterations; ++i) simulation.tick();
+
+    text
+      .attr('x', (_, i) => labels[i].x)
+      .attr('y', (_, i) => labels[i].y)
+      .each(function (_, i) {
+        const a = Math.atan2(labels[i].y - points[i].fy, labels[i].x - points[i].fx);
+        d3.select(this).call(
+          a > Math.PI / 4 && a <= Math.PI * 3 / 4 ? bottom
+            : a > -Math.PI / 4 && a <= Math.PI / 4 ? left
+              : a > -Math.PI * 3 / 4 && a <= Math.PI * 3 / 4 ? top
+                : right);
+      });
+  }
+
+  svg.append('g').attr('class', 'axis-x').call(xAxis);
+  svg.append('g').attr('class', 'axis-y').call(yAxis);
+
+  svg.append('g')
+    .attr('class', 'circles')
+    .selectAll('circle')
+    .data(data)
+    .enter().append('circle')
+    .attr('cx', d => x(d.x))
+    .attr('cy', d => y(d.y))
+    .attr('r', 4);
+
+  svg.append('g')
+    .attr('class', 'text')
+    .selectAll('text')
+    .data(data)
+    .enter().append('text')
+    .attr('x', d => x(d.x))
+    .attr('y', d => y(d.y))
+    .text(d => d.name)
+    .call(dodge);
 }
 
-export function drawLines(id, width, height, data) {
-  return undefined;
+export function drawLines(id, widthMain, heightMain) {
+  const svg = makeSVG(id, widthMain, heightMain);
+
+  const
+    margin = { top: 50, right: 50, bottom: 50, left: 50 },
+    width = widthMain - margin.left - margin.right,
+    height = heightMain - margin.top - margin.bottom;
+
+  const num = 21; // The number of datapoints
+
+  const xScale = d3.scaleLinear()
+    .domain([0, num - 1])
+    .range([0, width]);
+
+  const yScale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([height, 0]);
+
+  const line = d3.line()
+    .x((_, i) => xScale(i))
+    .y((d) => yScale(d.y))
+    .curve(d3.curveMonotoneX);
+
+
+  const dataset = d3.range(num).map(() => { return { 'y': d3.randomUniform(1)() }; });
+
+  svg
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+  svg.append('g')
+    .attr('transform', `translate(0,${height})`)
+    .call(d3.axisBottom(xScale));
+
+  svg.append('g')
+    .call(d3.axisLeft(yScale));
+
+  svg.append('path')
+    .datum(dataset)
+    .attr('class', 'line')
+    .attr('d', line);
+
+  svg.append('g').attr('class', 'dots').selectAll()
+    .data(dataset)
+    .enter().append('circle')
+    .attr('cx', (d, i) => xScale(i))
+    .attr('cy', (d) => yScale(d.y))
+    .attr('r', 5);
 }
 
 export function drawMap(id, width, height, data, population) {
@@ -36,12 +158,11 @@ export function drawMap(id, width, height, data, population) {
     .domain([10, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1500000].map(n => n * 1000))
     .range(['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#08519c', '#08306b', '#03132b']);
 
-  const projection = d3.geoEquirectangular()
-    .scale(width / 3)
+  const projection = d3.geoMercator()
+    .scale(width / 4)
     .translate([width / 2, height / 1.2]);
 
   const path = d3.geoPath().projection(projection);
-
   const zoom = d3.zoom()
     .scaleExtent([1, 6])
     .on('zoom', zoomed);
@@ -105,7 +226,9 @@ export function drawChord(id, width, height, data) {
     .attr('font-size', 10)
     .attr('font-family', 'sans-serif');
 
-  const chords = d3.chord(chordData);
+  const myChord = d3.chord().padAngle(0.05).sortSubgroups(d3.descending);
+
+  const chords = myChord(data);
   const ribbon = d3.ribbon().radius(innerRadius);
 
   const formatValue = d3.formatPrefix(',.0', 1e3);
@@ -117,7 +240,11 @@ export function drawChord(id, width, height, data) {
     .innerRadius(innerRadius)
     .outerRadius(outerRadius);
 
-  const group = svg.append('g').selectAll('g').data(chords.groups).enter().append('g');
+
+  const group = svg.append('g')
+    .selectAll('g')
+    .data(chords.groups)
+    .enter().append('g');
 
   group.append('path')
     .attr('fill', d => color(d.index))
--- src/index.js
+++ src/index.js
@@ -11,15 +11,50 @@ import './idioms.css';
 
 // FETCH DATA AND DRAW
 
+const chordData = [
+  [19, 11, 19, 14, 15, 14, 13, 17],
+  [5, 9, 5, 13, 0, 16, 2, 9],
+  [12, 18, 12, 2, 1, 6, 13, 8],
+  [11, 12, 18, 5, 19, 6, 16, 18],
+  [19, 20, 17, 7, 16, 5, 9, 12],
+  [13, 18, 15, 6, 12, 14, 11, 7],
+  [10, 5, 0, 19, 18, 1, 1, 5],
+  [20, 10, 7, 7, 13, 6, 1, 20]];
+
+const plotData = [
+  { 'name': 'Mazda', 'x': 21, 'y': 110 },
+  { 'name': 'Datsun', 'x': 22.8, 'y': 93 },
+  { 'name': 'Hornet', 'x': 18.7, 'y': 175 },
+  { 'name': 'Duster', 'x': 14.3, 'y': 245 },
+  { 'name': 'Merc', 'x': 24.4, 'y': 62 },
+  { 'name': 'Chrysler', 'x': 14.7, 'y': 230 },
+  { 'name': 'Fiat', 'x': 32.4, 'y': 66 },
+  { 'name': 'Honda', 'x': 30.4, 'y': 52 },
+  { 'name': 'Toyota', 'x': 33.9, 'y': 65 },
+  { 'name': 'Dodge', 'x': 15.5, 'y': 150 },
+  { 'name': 'AMC', 'x': 15.2, 'y': 150 },
+  { 'name': 'Camaro', 'x': 13.3, 'y': 245 },
+  { 'name': 'Pontiac', 'x': 19.2, 'y': 175 },
+  { 'name': 'Fiat-9', 'x': 27.3, 'y': 66 },
+  { 'name': 'Porsche-2', 'x': 26, 'y': 91 },
+  { 'name': 'Lotus', 'x': 30.4, 'y': 113 },
+  { 'name': 'Ford L', 'x': 15.8, 'y': 264 },
+  { 'name': 'Ferrari', 'x': 19.7, 'y': 175 },
+  { 'name': 'Maserati', 'x': 15, 'y': 335 },
+  { 'name': 'Volvo', 'x': 21.4, 'y': 109 }];
+
+plotData.x = 'Miles per gallon';
+plotData.y = 'Horsepower';
+
 Promise.all([
   json('./data/countries.json'),
   tsv('./data/population.tsv'),
 ]).then((dataResults) => {
   const [countries, population] = dataResults;
-  idioms.drawPlot('#plot', 600, 400);
-  idioms.drawLines('#lines', 600, 400);
-  idioms.drawMap('#map', 800, 400, countries, population);
-  // idioms.drawChord(svgChord);
+  idioms.drawPlot('#plot', 860, 420, plotData);
+  idioms.drawLines('#lines', 860, 420);
+  idioms.drawMap('#map', 860, 420, countries, population);
+  idioms.drawChord('#chord', 600, 420, chordData);
 });
 
 // UPDATE DATA HANDLERS
