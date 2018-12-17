e8cbc2abc1df1e262fbd07910e843baae966035e
Bruno <zettca@gmail.com>
2018-12-17 05:40:24 +0000

    Updates

---
+++ precache-manifest.82c0fb421402c33ee71e3bd010316ffb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "335303df91e90dc68f37",
+    "url": "./static/js/main.335303df.chunk.js"
+  },
+  {
+    "revision": "e608ea985a84b5eb8726",
+    "url": "./static/js/1.e608ea98.chunk.js"
+  },
+  {
+    "revision": "335303df91e90dc68f37",
+    "url": "./static/css/main.d86b2956.chunk.css"
+  },
+  {
+    "revision": "e608ea985a84b5eb8726",
+    "url": "./static/css/1.5b05b956.chunk.css"
+  },
+  {
+    "revision": "59e7a6c072a9a1e0a1fdc2f718b7d8d7",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.527769dab20727ad1702bdc2d2a857ff.js"
+  "./precache-manifest.82c0fb421402c33ee71e3bd010316ffb.js"
 );
 
 workbox.clientsClaim();
